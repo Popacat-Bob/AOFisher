c1b3ef1a65ba16a0ca21398f7cb8d36f385533ba
Popacat-Bob <gdloyola9@gmail.com>
2025-07-16 12:59:00 +0800

    added

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from pyautogui import alert
+from pyautogui import alert, click
+from time import sleep
 
 class fisherModel:
-    
+
     def __init__(self):
         self.screenTopLeft: int = -1
         self.screenBotRight: int = -1
 
     def catch(self, isPrompted: bool):
+
         if self.screenTopLeft < 0 and self.screenBotRight < 0:
-            alert(text= "Fishing screen not set", title="alert", button="ok")
+            raise ValueError("Screen Coordinates not set")
+
+        if isPrompted:
+
+            for i in range(5):
+                click()
+                sleep(0.5)
+
 
