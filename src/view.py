894e1a8de24127ee22c3248aa52c7a3a885be09f
Popacat-Bob <gdloyola9@gmail.com>
2025-07-16 20:45:44 +0800

    hi

--- src/view.py
+++ src/view.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QIcon
 
 class view(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("AO Fisher")
         self.setGeometry(0, 0, 500, 500)
+        self.setWindowIcon(QIcon('icons/fish.png'))
