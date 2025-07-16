42fe3a3e3a4b6aac66e0be3b76ca62e0814268d9
Popa <gdloyola9@gmail.com>
2025-07-16 05:15:19 -0700

    debugged

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from controller import controller
-from init import fishingModel, view
+from init import fishingModel
 
 def main():
-    control = controller(fishingModel, view)
+    control = controller(fishingModel)
     control.run()
 
 if __name__ == '__main__':
