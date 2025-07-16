e7d717b12b45902c33b62e1beb5002201d33c1db
Popacat-Bob <gdloyola9@gmail.com>
2025-07-16 16:28:27 +0800

    hi

---
+++ init.py
@@ -0,0 +1,9 @@
+from model import model, colorCaptureModel
+
+colorCapture = colorCaptureModel((100, 100),
+                                 ( 400, 400,),
+                                 10
+                                 )
+fishingModel = model(colorCapture,
+                     (255, 255, 255),
+                     )
